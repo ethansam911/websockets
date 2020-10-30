1bfb5158625174b3fb0788589005fadb5846a2fc
Ethan <esam5556@gmail.com>
2020-10-30 18:30:05 -0400

    Implemented tick tock app and removed streams to remove null statement

--- channelsproj/channelsproj/routing.py
+++ channelsproj/channelsproj/routing.py
@@ -1,11 +1,13 @@
 from channels.routing import ProtocolTypeRouter, URLRouter 
 from django.urls import path 
 
-from notifier.consumers import EchoConsumer
+# from notifier.consumers import EchoConsumer
+from notifier.consumers import TickTockConsumer
 
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
-        path("ws/", EchoConsumer),
+        # path("ws/", EchoConsumer),
+         path("ws/", TickTockConsumer),
     ])
 })
--- channelsproj/notifier/consumers.py
+++ channelsproj/notifier/consumers.py
@@ -1,15 +1,27 @@
-from channels.consumer import AsyncConsumer
+import asyncio
+# Automatically handles sending json objects
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-class EchoConsumer(AsyncConsumer):
-    async def websocket_connect(self,event):
-        await self.send({
-            "type": "websocket.accept"
-        })
+# class EchoConsumer(AsyncConsumer):
+#     async def websocket_connect(self,event):
+#         await self.send({
+#             "type": "websocket.accept"
+#         })
     
-    async def websocket_receive(self, event):
-        #Echo the recieved payload
-        #sending something over a client network is a blocking activity
-        await self.send({
-            "type": "websocket.send",
-            "text": event["text"]
-        })
+#     async def websocket_receive(self, event):
+#         #Echo the recieved payload
+#         #sending something over a client network is a blocking activity
+#         await self.send({
+#             "type": "websocket.send",
+#             "text": event["text"]
+#         })
+
+
+class TickTockConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        while 1:
+            await asyncio.sleep(1)
+            await self.send_json("tick")
+            await asyncio.sleep(1)
+            await self.send_json(".......tock")
